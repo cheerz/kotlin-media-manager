d10e987043c0f6ab4eaa0387aa4858c6d267849e
Guillaume Belouin <applichic@gmail.com>
2024-05-31 12:10:00 +0200

    Download from google cloud

--- src/main/kotlin/com/cheerz/mediamanager/routes/Home.kt
+++ src/main/kotlin/com/cheerz/mediamanager/routes/Home.kt
@@ -1,6 +1,5 @@
 package com.cheerz.mediamanager.routes
 
-import com.cheerz.mediamanager.storage
 import io.ktor.server.application.call
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
